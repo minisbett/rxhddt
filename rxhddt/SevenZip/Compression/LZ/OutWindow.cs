651b3105808a5625a553b32a32157f891cc220fc
Niklas Fehde <niklas.fehde@gmx.de>
2021-04-10 12:37:23 +0200

    removed the jetbrains decompiler comments

--- rxhddt/SevenZip/Compression/LZ/OutWindow.cs
+++ rxhddt/SevenZip/Compression/LZ/OutWindow.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: SevenZip.Compression.LZ.OutWindow
-// Assembly: osu!ftw, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 43BAB63D-DE2A-4E38-86D4-2EF193F865F8
-// Assembly location: C:\Users\Niklas\Downloads\aac_Portable\osu!\osu!ftw.exe
-
-using System.IO;
+﻿using System.IO;
 
 namespace SevenZip.Compression.LZ
 {
